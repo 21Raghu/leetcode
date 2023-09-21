4d8210a920066ac730546074b3c540699b4b6eaf
Raghavendra Singh Chouhan <21raghuchouhan@gmail.com>
2023-09-21 20:53:20 +0530

    Update rev_polish_notation.py

--- rev_polish_notation.py
+++ rev_polish_notation.py
@@ -1,23 +1,20 @@
-#tokens = ["2", "1", "+", "3", "*"][::-1]
-tokens =["10","6","9","3","+","-11","*","/","*","17","+","5","+"][::-1]
-st = []
-res = []
-op = 0
-for ch in tokens:
-    if ch in '-+/*':
-        st.append(ch)
-    elif len(st) > 0:
-        op+=1
-        res.append('(')
-        res.append(ch + st.pop())
-    else:
-        res.append(ch)
-        res.append(')')
-        op-=1
-print(op)
-while op:
-    res.append(')')
-    op-=1
-print(res)
-x = "".join(res)
-print(x,eval(x))
+def evalRPN(tokens):
+    stack = []
+    for t in tokens:
+        if t not in {"+", "-", "*", "/"}:
+            stack.append(int(t))
+        else:
+            b, a = stack.pop(), stack.pop()
+            if t == "+":
+                stack.append(a + b)
+            elif t == "-":
+                stack.append(a - b)
+            elif t == "*":
+                stack.append(a * b)
+            else:
+                stack.append(a / b)
+    return stack[0]
+
+tok = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+print(evalRPN(tok))
+
