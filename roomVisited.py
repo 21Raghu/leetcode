dee83c0bd9845554aa85360f794f949bfa61c435
Raghavendra Singh Chouhan <21raghuchouhan@gmail.com>
2023-10-08 18:12:20 +0530

    Create roomVisited.py

---
+++ roomVisited.py
@@ -0,0 +1,21 @@
+
+
+rooms = [[1, 3], [3, 0, 1], [], [2]]
+
+
+def roomvisit(rooms):
+    vist = set()
+    st = [0]
+    while st:
+        room = st.pop()
+        vist.add(room)
+        for key in rooms[room]:
+            if key in vist:
+                st.append(key)
+    return len(vist) == len(rooms)
+
+
+
+
+
+print(roomvisit(rooms))
